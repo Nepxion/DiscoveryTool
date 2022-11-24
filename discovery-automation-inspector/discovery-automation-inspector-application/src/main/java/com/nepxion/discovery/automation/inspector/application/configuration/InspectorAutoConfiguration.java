6e05667ec2d2e9062ff2254ef4c0370a29754899
HaojunRen <1394997@qq.com>
2022-11-24 08:12:50 +0800

    Initialize discovery-automation modules

---
+++ discovery-automation-inspector/discovery-automation-inspector-application/src/main/java/com/nepxion/discovery/automation/inspector/application/configuration/InspectorAutoConfiguration.java
@@ -0,0 +1,29 @@
+package com.nepxion.discovery.automation.inspector.application.configuration;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.nepxion.discovery.automation.inspector.entity.InspectorTestCaseCondition;
+import com.nepxion.discovery.automation.inspector.entity.InspectorTestCaseProperty;
+
+@Configuration
+public class InspectorAutoConfiguration {
+    @Bean
+    public InspectorTestCaseProperty testCaseProperty() {
+        return new InspectorTestCaseProperty();
+    }
+
+    @Bean
+    public InspectorTestCaseCondition testCaseCondition() {
+        return InspectorTestCaseCondition.fromInspectorFile();
+    }
+}
