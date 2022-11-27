69f2e6c96560f41bfd996345b96250b17da72a14
HaojunRen <1394997@qq.com>
2022-11-27 22:15:29 +0800

    Add logo

---
+++ discovery-automation-concurrent/discovery-automation-concurrent-starter-redisson/src/main/java/com/nepxion/discovery/automation/concurrent/redisson/context/RedissonApplicationContextInitializer.java
@@ -0,0 +1,31 @@
+package com.nepxion.discovery.automation.concurrent.redisson.context;
+
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nepxion.banner.BannerConstant;
+import com.nepxion.banner.Description;
+import com.nepxion.banner.LogoBanner;
+import com.nepxion.banner.NepxionBanner;
+import com.taobao.text.Color;
+
+public class RedissonApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        if (!(applicationContext instanceof AnnotationConfigApplicationContext)) {
+            LogoBanner logoBanner = new LogoBanner(RedissonApplicationContextInitializer.class, "/com/nepxion/redisson/resource/logo.txt", "Welcome to Nepxion", 8, 5, new Color[] { Color.red, Color.green, Color.cyan, Color.blue, Color.yellow, Color.magenta, Color.red, Color.green }, true);
+
+            NepxionBanner.show(logoBanner, new Description("Lock:", "Redisson", 0, 1), new Description(BannerConstant.GITHUB + ":", BannerConstant.NEPXION_GITHUB + "/Discovery", 0, 1));
+        }
+    }
+}
