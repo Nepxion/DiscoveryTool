016ea17abcabdfb25c66a93086d77891fcbfd26b
HaojunRen <1394997@qq.com>
2022-12-23 22:35:07 +0800

    Add startup.exe.bat

---
+++ discovery-automation-inspector/discovery-automation-inspector-application/src/assembly/startup.exe.bat
@@ -0,0 +1,20 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                Nepxion Discovery Inspector                $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Studio All Right Reserved                        $
+@echo $  Copyright (C) 2017-2050                                  $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Nepxion Discovery Inspector
+@color 0a
+
+call discovery-automation-inspector.exe -Dnepxion.banner.shown.ansi.mode=true
+
+pause
